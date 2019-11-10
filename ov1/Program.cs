7ad29b16c602ab37913a96e60de7b17deadde716
nkarij <nannakari@gmail.com>
2019-11-10 09:21:23 +0100

    ov1

---
+++ ov1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ov1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // en class attribut?
+            List<string> animals = new List<string>();
+
+            // kunne være en metode, addAnimalToList
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Name an animal:");
+                string animalName = Console.ReadLine();
+                animals.Add(animalName);
+            }
+
+            // kunne være en metode, printAnimalList
+            Console.WriteLine("Here is a list of your Animals:");
+            foreach (string animalname in animals)
+            {
+                Console.WriteLine(animalname);
+            }
+
+        }
+    }
+}
